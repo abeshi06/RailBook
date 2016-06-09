5a3565d8546296a996a1e499872bb33d1a507934
jhsAbe <abeshi06@hotmail.co.jp>
2016-06-09 16:45:44 +0900

    検証機能の追加

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,2 +1,5 @@
 class Book < ActiveRecord::Base
+  validates :isbn, presence: true, uniqueness: true
+  validates :title, length: {minimum: 1, maximum: 100}
+  validates :price, numericality: {only_integer: true}
 end
